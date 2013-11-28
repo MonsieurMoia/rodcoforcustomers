4d3138fbc2c70dd6ba53059b8119c4b20f76e0ba
Roberto Moia <moiacr@gmail.com>
2013-11-28 13:27:18 -0600

    angular working

--- _/components/js/myscript.js
+++ _/components/js/myscript.js
@@ -1,2 +1,34 @@
-var customerapp = angular.module('customerapp', []);
+var customerapp= angular.module('customerapp', []);
 
+//Define Routing for app
+//Uri /AddNewOrder -> template AddOrder.html and Controller AddOrderController
+//Uri /ShowOrders -> template ShowOrders.html and Controller AddOrderController
+customerapp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+    	when('/productos', {
+			templateUrl: 'productos.html',
+			controller: 'productosController'
+    	}).
+    	when('/confirmarorden', {
+			templateUrl: 'confirmarorden.html',
+			controller: 'confirmarordenController'
+    	}).
+      	otherwise({
+			redirectTo: '/productos'
+    });
+}]);
+
+
+customerapp.controller('productosController', function($scope) {
+	
+	
+	
+});
+
+
+customerapp.controller('confirmarordenController', function($scope) {
+
+	
+
+});
